923dc14f8a408bd1933f4d9b1cd3ef436ff0a4bf
jfdreis <josevtnreis@gmail.com>
2023-09-17 00:26:21 +0100

    More elegant code

--- set_1_basics/src/main.rs
+++ set_1_basics/src/main.rs
@@ -15,20 +15,25 @@ use base64::{Config, CharacterSet, encode_config};
 
 fn main() {
     // Will  input the original hexadecimal string
-
     let s= "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     println!("The original hexadecimal string: {}", s);
+    
+    //use the function we defined to convert from hex to based64
+    let result= hex_to_base64(s.to_string());
+    println!("Base64 encoded: {}", result)
+}
+
+fn hex_to_base64 (s: String)-> String {
     //Convert this string to bytes.
     //Note that decode returns a a Result<Vec<u>> we need to take handle the error case.
     let bytes = match hex::decode(s){
         Ok(bytes) => bytes,
-        Err(e) => {
-            println!("Error decoding hexadecimal string:{}", e);
-            return;
+        Err(_) => {
+            let a = String::from("Error decoding hexadecimal string: {}");
+            return a;
         }
     };
     //Convert from bytes to base64
-
     let s_in_base64 = encode_config(&bytes, Config::new(CharacterSet::Standard, false));
-    println!("Base64 encoded: {}",s_in_base64);
+    String::from(s_in_base64)
 }
