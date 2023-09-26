b8ee88cd12cc4ab8dbfe0aa88f512689f22ae138
jfdreis <josevtnreis@gmail.com>
2023-09-26 18:38:37 +0100

    Solved cryptopals Challenge 3 set 1. Did not use their hint

---
+++ set_1_basics/src/lib.rs
@@ -0,0 +1,95 @@
+use base64::{Config, CharacterSet, encode_config};
+use std::str;
+
+
+pub fn hex_to_base64 (s: &str)-> String { //want &str because I do not need ownership of the string
+    //Convert this string to bytes.
+    //Note that decode returns a a Result<Vec<u>> we need to take handle the error case.
+    let bytes = match hex::decode(s){
+        Ok(bytes) => bytes,
+        Err(_) => {
+            let a = String::from("Error decoding hexadecimal string: {}");
+            return a;
+        }
+    };
+    //Convert from bytes to base64
+    let s_in_base64 = encode_config(&bytes, Config::new(CharacterSet::Standard, false));
+    String::from(s_in_base64)
+}
+
+pub fn fixed_xor(s: &str, t:  &str) -> String {
+    //convert hexadecimal to bytes
+    let s_bytes = hex::decode(&s).expect("Invalid hexadecimal base input");
+    let t_bytes = hex::decode(&t).expect("Invalid hexadecimal base input");
+    
+    // Perform the XOR operation on each pair of bytes
+    let xor_result: Vec<u8> = s_bytes.iter().zip(&t_bytes).map(|(a, b)| a ^ b).collect();    
+    
+    // Convert the result back to a hexadecimal string
+    let xor_hex: String = xor_result.iter().map(|byte| format!("{:02x}", byte)).collect();
+    xor_hex
+   
+}
+
+pub fn hex_to_ascii(s: &str) -> String {
+    let mut ascii_values = String::new();
+    let mut hex_buffer = String::new(); // Store two hexadecimal characters.
+
+    for hex_char in s.chars() {
+        hex_buffer.push(hex_char);
+
+        // When we have two hexadecimal characters in the buffer, parse and convert to ASCII.
+        if hex_buffer.len() == 2 {
+            //println!("{hex_buffer}");
+            if let Ok(byte) = u8::from_str_radix(&hex_buffer, 16) {
+                ascii_values.push(byte as char);
+            } else {
+                return String::from("Error converting hexadecimal to ASCII values");
+            }
+
+            hex_buffer.clear(); // Clear the buffer for the next pair.
+        }
+    }
+
+    ascii_values
+}
+
+pub fn xor_hexa_bytes_get_bytes(s: &str, v: &Vec<u8>)-> Vec<u8>{
+    let s_bytes = hex::decode(&s).expect("Invalid hexadecimal base input");
+    let ans: Vec<u8> = s_bytes.iter().zip(v).map(|(a, b)| a ^ b).collect();
+    ans
+}
+
+pub fn xor_hexa_bytes_get_string(s: &str, v: &Vec<u8>)-> String{
+    let s_bytes = hex::decode(&s).expect("Invalid hexadecimal base input");
+    let ans: Vec<u8> = s_bytes.iter().zip(v).map(|(a, b)| a ^ b).collect();
+    let t = match str::from_utf8(&ans) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    t.to_string()
+}
+
+
+//This function get a string in hexadecimal and determines the percentage
+// of the symbols that correspond to lowercase letter and space
+
+pub fn percentage_letters_in_text(v: Vec<u8>)-> f64 {
+    let mut letters: Vec<u8> =vec![32]; // it as the key for the space
+
+    // we do not add the keys for uppercase letters because most letters in a sentence are lowercase
+    //we add lowercase letters
+
+    for letter in 97..=122 {
+        letters.push(letter);
+    }
+
+    let mut a=0.0;
+    for value in &v {
+        if letters.contains(value) {
+            a = a + 1.0;
+        }
+    }
+    a/v.len() as f64
+}
+
--- set_1_basics/src/main.rs
+++ set_1_basics/src/main.rs
@@ -1,7 +1,4 @@
-//use std::string;
-
-//use hex::FromHEx;
-use base64::{Config, CharacterSet, encode_config};
+use set_1_basics::*;
 
 fn main() {
     //Here is the solution for the first challenge of Set 1
@@ -9,50 +6,73 @@ fn main() {
 
     println!("Challenge 1 of Set 1");
     let s= "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    println!("The original hexadecimal string: {}", s);
-    
+    println!("The original hexadecimal string: {}", s);    
     //use the function we defined to convert from hex to based64
-    let result= hex_to_base64(s.to_string());
+    let result= hex_to_base64(s);
     println!("Base64 encoded: {}", result);
+    // convert to ascii
+    let s_ascii=    hex_to_ascii(s);
+    println!("The original string in ascii: {}",s_ascii);
 
     println!("Challenge 2 of Set 1");
 
     // Input
-    let s= String::from("1c0111001f010100061a024b53535009181c");
-    let t = String::from("686974207468652062756c6c277320657965");
+    let s ="1c0111001f010100061a024b53535009181c";
+    let t = "686974207468652062756c6c277320657965";
     println!("We are going to do the XOR combination of {} and {}.",s,t);
+    println!("The original strings in ascii values: {} AND {}", hex_to_ascii(s),hex_to_ascii(t));
     let ans= fixed_xor(s,t);
     println!("The solution to challenge 2 is {}",ans);
-}
+    println!("The solution in ascii values is {}", hex_to_ascii(&ans));
 
+    println!("Challenge 3 of Set 1");
 
+    //string that I want to decipher
+    let s="1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    
+    //I am going to make a vector whose entries are u8 values corresponding to space or letters
+    let mut letters: Vec<u8> =vec![32]; // it as the key for the space
 
-fn hex_to_base64 (s: String)-> String {
-    //Convert this string to bytes.
-    //Note that decode returns a a Result<Vec<u>> we need to take handle the error case.
-    let bytes = match hex::decode(s){
-        Ok(bytes) => bytes,
-        Err(_) => {
-            let a = String::from("Error decoding hexadecimal string: {}");
-            return a;
+    for letter in 65..=90 {     //adding the keys for uppercase letters
+        letters.push(letter);
+    }
+    for letter in 97..=122 {     //adding the keys for lowercase letters
+        letters.push(letter);
+    }
+
+    //Now as the  string that  we want to decipher was xor against a caracther we will see what happens when we xor "s" agains letters.
+    //If after XORing we get a high percentage of letter, we save that character
+    let mut possibilities:Vec<u8> =vec![];
+    for i in letters {
+        let mut key_v: Vec<u8> =vec![i];
+        while key_v.len() < s.len()/2 {
+            key_v.push(i);
+        };
+        
+        let t= xor_hexa_bytes_get_bytes(s,&key_v);
+        let tax=percentage_letters_in_text(t);
+        if tax > 0.7 {
+            possibilities.push(i);
         }
-    };
-    //Convert from bytes to base64
-    let s_in_base64 = encode_config(&bytes, Config::new(CharacterSet::Standard, false));
-    String::from(s_in_base64)
+    }
+    println!("The possible key(s) is(are) {:?}",possibilities);
+
+    // Here we print everything that we obtain while XORing against the entries in possibilities
+    for i in possibilities {
+        let mut key_v: Vec<u8> =vec![i];
+        while key_v.len() < s.len()/2 {
+            key_v.push(i);
+        };
+        let u= xor_hexa_bytes_get_string(s,&key_v);
+        println!("For key {} the result is result: {:?}", i, u);
+    }
 }
+    
+
+    
 
 
-fn fixed_xor(s: String, t:  String) -> String {
-    //convert hexadecimal to bytes
-    let s_bytes = hex::decode(&s).expect("Invalid hexadecimal base input");
-    let t_bytes = hex::decode(&t).expect("Invalid hexadecimal base input");
-    
-    // Perform the XOR operation on each pair of bytes
-    let xor_result: Vec<u8> = s_bytes.iter().zip(&t_bytes).map(|(a, b)| a ^ b).collect();    
-    
-    // Convert the result back to a hexadecimal string
-    let xor_hex: String = xor_result.iter().map(|byte| format!("{:02x}", byte)).collect();
-    xor_hex
-   
-}
+
+
+
+
